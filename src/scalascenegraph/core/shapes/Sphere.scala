8215daa281cde1d882c8d277bb23c2c957ac3ddf
Vincent Gay <glurbi@gmail.com>
2010-08-20 17:51:59 +0200

    mouarfff 4

--- src/scalascenegraph/core/shapes/Sphere.scala
+++ src/scalascenegraph/core/shapes/Sphere.scala
@@ -34,32 +34,25 @@ class SphereBuilder(n: Int, r: Float) {
 
 	implicit def doubleToFloat(d: Double): Float = d.asInstanceOf[Float]
 	
+	private def sphere(teta: Float, phi: Float): Vertice = {
+		// cf http://en.wikipedia.org/wiki/Sphere
+		val x = r * sin(teta) * cos(phi)
+		val y = r * sin(teta) * sin(phi)
+		val z = r * cos(teta)
+		Vertice(x, y, z)
+	}
+	
 	def createVertices = {
 		val ab = new ArrayBuffer[Float]
 		val stepAngle = Pi / n
-		
-		for (latStep <- 0 to n) {
-			for (lonStep <- 0 to 2*n) {
-				
-				val teta = latStep * stepAngle
-				val phi = lonStep * stepAngle
-				
-				ab += (sin(teta) * cos(phi))
-				ab += (sin(teta) * sin(phi))
-				ab += (cos(teta))
-				
-				ab += (sin(teta+stepAngle) * cos(phi))
-				ab += (sin(teta+stepAngle) * sin(phi))
-				ab += (cos(teta+stepAngle))
-				
-				ab += (sin(teta+stepAngle) * cos(phi+stepAngle))
-				ab += (sin(teta+stepAngle) * sin(phi+stepAngle))
-				ab += (cos(teta+stepAngle))
-				
-				ab += (sin(teta) * cos(phi+stepAngle))
-				ab += (sin(teta) * sin(phi+stepAngle))
-				ab += (cos(teta))
-				
+		for (tetaStep <- 0 to n) {
+			for (phiStep <- 0 to 2*n) {
+				val teta = tetaStep * stepAngle
+				val phi = phiStep * stepAngle
+				ab ++= sphere(teta, phi).asFloatArray
+				ab ++= sphere(teta+stepAngle, phi).asFloatArray
+				ab ++= sphere(teta+stepAngle, phi+stepAngle).asFloatArray
+				ab ++= sphere(teta, phi+stepAngle).asFloatArray
 			}
 		}
 		Vertices(Buffers.newDirectFloatBuffer(ab.toArray))
