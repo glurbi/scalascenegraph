7a416436834efa75f594d3a23df1ec5d1a4ff2db
Vincent Gay <glurbi@gmail.com>
2010-09-07 14:39:40 +0200

    waiting file...

---
+++ src/scalascenegraph/examples/Example9.scala
@@ -0,0 +1,18 @@
+package scalascenegraph.examples
+
+import java.awt._
+import java.awt.{Color => JColor}
+import java.awt.{Font => JFont}
+import java.awt.image._
+
+import scalascenegraph.core._
+import scalascenegraph.core.Predefs._
+import scalascenegraph.dsl._
+
+class Example9 extends Example with WorldBuilder {
+
+	def example =
+		world {
+		}
+	
+}
