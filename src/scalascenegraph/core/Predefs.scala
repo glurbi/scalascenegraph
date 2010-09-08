da1e41a0fbc26e2a8829fab3421882ef1650c3b8
Vincent Gay <glurbi@gmail.com>
2010-09-08 11:04:32 +0200

    a little bit of doc

--- src/scalascenegraph/core/Predefs.scala
+++ src/scalascenegraph/core/Predefs.scala
@@ -4,44 +4,80 @@ import java.nio._
 
 object Predefs {
 
+	/**
+	 * A Hook defines a callback function that can be called at different stage
+	 * of the rendering. 
+	 */
 	type Hook[T] = (T, Context) => Unit
+	
+	/**
+	 * A Hook that takes a Node and a Context in parameter.
+	 */
 	type NodeHook[T <: Node] = Hook[T]
+	
+	/**
+	 * A Hook that takes a State and a Context in parameter.
+	 */
 	type StateHook[T <: State] = Hook[T]
 	
+	/**
+	 * Defines the shade model used for rendering.
+	 */
 	abstract class ShadeModel
 	case object Flat extends ShadeModel
 	case object Smooth extends ShadeModel
 	
+	/**
+	 * Defines the "active" face of a polygon. 
+	 */
 	abstract class Face
 	case object Front extends Face
 	case object Back extends Face
 	case object FrontAndBack extends Face
 
+	/**
+	 * Defines a light type.
+	 */
 	abstract class LightType
 	case object AmbientLight extends LightType
 	case object DiffuseLight extends LightType
 	case object SpecularLight extends LightType
 	case object EmissionLight extends LightType
 	case object AmbientAndDiffuseLight extends LightType
-	
+
+	/**
+	 * Defines a polygon drawing mode.
+	 */
 	abstract class DrawingMode
 	case object Point extends DrawingMode
 	case object Line extends DrawingMode
 	case object Fill extends DrawingMode
 
+	/**
+	 * Defines how the front and back face of a polygon are calculated. 
+	 */
 	abstract class FrontFace
 	case object ClockWise extends FrontFace
 	case object CounterClockWise extends FrontFace
 	
+	/**
+	 * Defines a state that can have on/off as a value.
+	 */
 	abstract class OnOffState
 	case object On extends OnOffState
 	case object Off extends OnOffState
 
+	/**
+	 * Defines the equation used for calculating the fog component.
+	 */
 	abstract class FogMode
 	case class Linear(start: Float, end: Float) extends FogMode
 	case class Exp(density: Float) extends FogMode
 	case class Exp2(density: Float) extends FogMode
 	
+	/**
+	 * Defines the different light instances.
+	 */
 	class LightInstance(val n: Int)
 	object LightInstance {
 		def apply(n: Int): LightInstance = new LightInstance(n)
@@ -56,47 +92,94 @@ object Predefs {
 	case object Light6 extends LightInstance(6)
 	case object Light7 extends LightInstance(7)
 	
+	/**
+	 * A 3D vector.
+	 */
 	case class Vector(x: Float, y: Float, z: Float) {
 		val asFloatArray = Array(x, y, z)
 	}
+	
+	/**
+	 * A 3D point. 
+	 */
 	case class Vertice(x: Float, y: Float, z: Float)  {
 		val asFloatArray = Array(x, y, z)
 	}
+	
+	/**
+	 * Surface normal vector.
+	 */
 	case class Normal(x: Float, y: Float, z: Float)  {
 		val asFloatArray = Array(x, y, z)
 	}
 	
+	/**
+	 * Holds a set of vertices.
+	 */
 	case class Vertices(floatBuffer: FloatBuffer) {
-		def count: Int = floatBuffer.limit /3
+		def count: Int = floatBuffer.limit / 3
 	}
+	
+	/**
+	 * A light position.
+	 */
 	case class Position(x: Float, y: Float, z: Float) {
 		def asFloatArray: Array[Float] = Array(x, y, z)
 	}
 	
+	/**
+	 * A light intensity.
+	 */
 	case class Intensity(r: Float, g: Float, b: Float, a: Float = 1.0f) {
 		def asFloatArray: Array[Float] = Array(r, g, b, a)
 	}
 
+	/**
+	 * A color.
+	 */
 	case class Color(r: Float, g: Float, b: Float, a: Float = 1.0f) {
 		val asFloatArray = Array(r, g, b, a)
 	}
 	
+	/**
+	 * A set of colors.
+	 */
 	case class Colors(floatBuffer: FloatBuffer)
+	
+	/**
+	 * A set of normals.
+	 */
 	case class Normals(floatBuffer: FloatBuffer)
+	
+	/**
+	 * A set of texture coordinates.
+	 */
 	case class TextureCoordinates(floatBuffer: FloatBuffer)
 	
+	/**
+	 * A texture identifier.
+	 */
 	case class TextureId(id: Any)
-	
+
+	/**
+	 * Implicit conversion of AWT colors to ScalaSceneGraph colors.
+	 */
 	implicit def toColor(c: java.awt.Color): Color = {
 		new Color(c.getRed/255.0f, c.getGreen/255.0f, c.getBlue/255.0f, c.getAlpha/255.0f)
 	}
 
+	/**
+	 * Defines a geometry type.
+	 */
 	abstract class GeometryType
 	case object PointArray extends GeometryType
 	case object LineArray extends GeometryType
 	case object TriangleArray extends GeometryType
 	case object QuadArray extends GeometryType
 
+	/**
+	 * Defines a color type.
+	 */
 	abstract class ColorType
 	case object UnspecifiedColor extends ColorType
 	case object UniColorNoAlpha extends ColorType
@@ -104,11 +187,17 @@ object Predefs {
 	case object MultiColorNoAlpha extends ColorType
 	case object MultiColorAlpha extends ColorType
 	
+	/**
+	 * Defines a normal type.
+	 */
 	abstract class NormalType
 	case object UnspecifiedNormal extends NormalType
 	case object UniNormal extends NormalType
 	case object MultiNormal extends NormalType
 	
+	/**
+	 * Defines an image type.
+	 */
 	abstract class ImageType
 	case object RGB extends ImageType
 	case object RGBA extends ImageType
