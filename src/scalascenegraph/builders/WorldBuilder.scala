a3bb543bc1942a280efd074a806a22339e1d13d1
Vincent Gay <glurbi@gmail.com>
2010-11-02 17:57:41 +0100

    extracted GraphBuilder from WorldBuilder

--- src/scalascenegraph/builders/WorldBuilder.scala
+++ src/scalascenegraph/builders/WorldBuilder.scala
@@ -21,89 +21,10 @@ import scalascenegraph.core._
 import scalascenegraph.builders._
 import scalascenegraph.core.Predefs._
 
-// TODO: separate into GeometryBuilder, GraphBuilder, ResourceBuilder, ...
+// TODO: separate into GeometryBuilder, ResourceBuilder, ...
 // TODO: should be some consistency with what has to be attached or not
-trait WorldBuilder {
+trait WorldBuilder extends GraphBuilder {
 
-	private val stack = new Stack[Node]
-	
-	def world(body: => Unit): World = {
-		stack.push(new World)
-		body
-		stack.pop.asInstanceOf[World]
-	}
-
-	def detached(body: => Unit): Group = {
-		val group = new Group
-		stack.push(group)
-		body
-		stack.pop.asInstanceOf[Group]
-	}
-
-	def attach(node: Node) {
-		stack.top.attach(node)
-	}
-	
-	def attach(group: Group) {
-		stack.top.attach(group)
-	}
-	
-	def attach(geometry: Geometry) {
-		stack.top.attach(geometry)
-	}
-
-	def attach(texture: Texture) {
-		stack.top.attach(texture)
-	}
-	
-	def attach(font: Font) {
-		stack.top.attach(font)
-	}
-	
-	def attach(shader: Shader) {
-		stack.top.attach(shader)
-	}
-	
-	def attach(program: Program) {
-		stack.top.attach(program)
-	}
-
-	def attach(uniform: Uniform) {
-		stack.top.attach(uniform)
-	}
-	
-	def attach[T <: Buffer](vbo: VertexBufferObject[T]) {
-		stack.top.attach(vbo)
-	}
-
-	def group(body: => Unit): Group = {
-		val group = new Group
-		stack.top.attach(group)
-		stack.push(group)
-		body
-		stack.pop.asInstanceOf[Group]
-	}
-
-
-	def conditional(condition: (Context) => Boolean)(body: => Unit): Node = {
-		val child = detached { body }
-		val optional = new Node {
-			def render(context: Context) {
-				if (condition(context)) {
-					child.render(context)
-				}
-			}
-		}
-		attach(optional)
-		optional
-	}
-
-	def node(fun: (Context) => Unit): Node = new Node {
-		def render(context: Context) {
-			fun(context)
-		}
-	}
-	
 	def color(c: Color) {
 		stack.top.attach(new ColorState(c))
 	}
