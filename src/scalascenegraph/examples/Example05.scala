02fa56fb3acfdcaf1a69fc87984722e280fad078
Vincent Gay <glurbi@gmail.com>
2010-11-08 15:52:13 +0100

    tmp

--- src/scalascenegraph/examples/Example05.scala
+++ src/scalascenegraph/examples/Example05.scala
@@ -15,11 +15,19 @@ import javax.media.opengl.fixedfunc.GLMatrixFunc._
 import scalascenegraph.core._
 import scalascenegraph.core.Predefs._
 import scalascenegraph.builders._
+import scalascenegraph.core.Utils._
 
 class Example05 extends Example with WorldBuilder {
 	
-	val angleHook = (r: Rotation, c: Context) => {
-		r.angle = (c.elapsed / 30.0f) % 360.0f
+	val rotation = (r: Rotation, c: Context) => {
+		r.angle = sin(c.elapsed / 180.0f) * 45.0f + 90.0f
+		val x = sin(c.elapsed / 180.0f)
+		val y = 1.0f
+		val z = cos(c.elapsed / 180.0f)
+		val rot = normalize(new Vector3D(x, y, z))
+		val r.x = rot.x
+		val r.y = rot.y
+		val r.z = rot.z
 	}
 	
 	def example =
@@ -28,38 +36,28 @@ class Example05 extends Example with WorldBuilder {
 			cullFace(On)
 			light(On)
 		    light(GL_LIGHT1, On)
-		    light(GL_LIGHT1, new Position3D(-3.0f, 0.0f, 0.0f))
+		    light(GL_LIGHT1, new Position3D(0.0f, 0.0f, 0.0f))
 		    light(GL_LIGHT1, GL_DIFFUSE, JColor.white)
 			group {
-				light(Off)
-				translation(-2.0f, 2.0f, -4.0f)
-				rotation(0.0f, 1.0f, 0.0f, 0.0f, angleHook)
-				polygonMode(GL_FRONT_AND_BACK, GL_LINE)
-				cullFace(Off)
-				attach(torus(30, 1.0f, 0.5f, normals = false))
-			}
-			group {
-				material(GL_FRONT, GL_DIFFUSE, JColor.orange)
-				translation(2.0f, -2.0f, -4.0f)
-				rotation(0.0f, 1.0f, 0.0f, 0.0f, angleHook)
-				attach(torus(30, 1.0f, 0.5f, normals = true))
-			}
-			group {
-				ambient(Intensity(0.3f, 0.3f, 0.3f, 1.0f))
-				material(GL_FRONT, GL_AMBIENT_AND_DIFFUSE, JColor.pink)
-				translation(-2.0f, -2.0f, -4.0f)
-				shadeModel(GL_FLAT)
-				rotation(0.0f, 1.0f, 0.0f, 0.0f, angleHook)
-				attach(torus(30, 1.0f, 0.5f, normals = true))
-			}
-			group {
-				light(GL_LIGHT1, GL_SPECULAR, JColor.white)
-				material(GL_FRONT, GL_DIFFUSE, JColor.red)
-				material(GL_FRONT, GL_SPECULAR, JColor.white)
-				shininess(GL_FRONT, 128)
-				translation(2.0f, 2.0f, -4.0f)
-				rotation(0.0f, 1.0f, 0.0f, 0.0f, angleHook)
-				attach(torus(30, 1.0f, 0.5f, normals = true))
+				group {
+					ambient(Intensity(0.3f, 0.3f, 0.3f, 1.0f))
+					material(GL_FRONT, GL_AMBIENT_AND_DIFFUSE, JColor.pink)
+					material(GL_FRONT, GL_SPECULAR, JColor.white)
+					shininess(GL_FRONT, 128)
+					translation(-2.0f, 0.0f, -4.0f)
+					shadeModel(GL_FLAT)
+					rotation(0.0f, 0.0f, 0.0f, 0.0f, rotation)
+					attach(torus(30, 1.0f, 0.5f, normals = true))
+				}
+				group {
+					light(GL_LIGHT1, GL_SPECULAR, JColor.white)
+					material(GL_FRONT, GL_AMBIENT_AND_DIFFUSE, JColor.red)
+					material(GL_FRONT, GL_SPECULAR, JColor.white)
+					shininess(GL_FRONT, 128)
+					translation(2.0f, 0.0f, -4.0f)
+					rotation(0.0f, 0.0f, 0.0f, 0.0f, rotation)
+					attach(torus(30, 1.0f, 0.5f, normals = true))
+				}
 			}
 		}
 	
